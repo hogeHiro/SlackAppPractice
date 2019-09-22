ee5cc9bddc5fbca2315244affe1513febe59f4c3
ryoji.ban <r3b3a0n@gmail.com>
2019-09-22 15:14:51 +0900

    メッセージに反応してメッセージを送信するように

--- volumes/app/app.js
+++ volumes/app/app.js
@@ -1,11 +1,20 @@
-const { App } = require('@slack/bolt');
+const { RTMClient } = require('@slack/rtm-api');
+const token = process.env.SLACK_BOT_TOKEN;
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+const rtm = new RTMClient(token);
+
+rtm.on('message', async (event) => {
+  console.log(event);
+  try {
+    // Send a welcome message to the same channel where the new member just joined, and mention the user.
+    const reply = await rtm.sendMessage(`Welcome to the channel, <@${event.user}>`, event.channel)
+    console.log('Message sent successfully', reply.ts);
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  // Connect to Slack
+  const { self, team } = await rtm.start();
 })();
